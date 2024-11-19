9b57aec1481e5b6f448a190e7f6f70224cdd5902
Julio Henrique <juliohsilvaramos@gmail.com>
2024-11-19 03:12:11 -0300

    Alteração 18/11/2024

--- src/main/java/com/projeto_final_backEnd/com/entities/Anamnese.java
+++ src/main/java/com/projeto_final_backEnd/com/entities/Anamnese.java
@@ -1,5 +1,6 @@
 package com.projeto_final_backEnd.com.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,15 +13,49 @@ public class Anamnese {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id_anamnese;
 	
+	@Column(nullable=false)
 	private boolean tem_lesao;
+	
+	@Column(nullable=false)
+	private String detalhe_lesao;
+	
+	@Column(nullable=false)
 	private String fator_limitante;
-	private boolean cirugia;
+	
+	@Column(nullable=false)
+	private boolean cirurgia;
+	
+	@Column(nullable=false)
+	private String detalhe_cirurgia;
+	
+	@Column(nullable=false)
 	private String desconforto_diario;
+	
+	@Column(nullable=false)
 	private String desconforto_exercicio;
+	
+	@Column(nullable=false)
 	private String alimentacao;
+	
+	@Column(nullable=false)
 	private boolean dieta;
+	
+	@Column(nullable=false)
+	private String detalhe_dieta;
+	
+	@Column(nullable=false)
 	private boolean orientacao_nutricional;
+	
+	@Column(nullable=false)
+	private String detalhe_nutri;
+	
+	@Column(nullable=false)
 	private boolean uso_ergogenico;
+	
+	@Column(nullable=false)
+	private String detalhe_ergogenico;
+	
+	@Column(nullable=false)
 	private Integer horas_sono;
 	
 	public Anamnese() {}
@@ -49,12 +84,12 @@ public class Anamnese {
 		this.fator_limitante = fator_limitante;
 	}
 
-	public boolean isCirugia() {
-		return cirugia;
+	public boolean isCirurgia() {
+		return cirurgia;
 	}
 
-	public void setCirugia(boolean cirugia) {
-		this.cirugia = cirugia;
+	public void setCirurgia(boolean cirugia) {
+		this.cirurgia = cirugia;
 	}
 
 	public String getDesconforto_diario() {
@@ -107,6 +142,46 @@ public class Anamnese {
 
 	public Integer getHoras_sono() {
 		return horas_sono;
+	}
+
+	public String getDetalhe_lesao() {
+		return detalhe_lesao;
+	}
+
+	public void setDetalhe_lesao(String detalhe_lesao) {
+		this.detalhe_lesao = detalhe_lesao;
+	}
+
+	public String getDetalhe_cirurgia() {
+		return detalhe_cirurgia;
+	}
+
+	public void setDetalhe_cirurgia(String detalhe_cirurgia) {
+		this.detalhe_cirurgia = detalhe_cirurgia;
+	}
+
+	public String getDetalhe_dieta() {
+		return detalhe_dieta;
+	}
+
+	public void setDetalhe_dieta(String detalhe_dieta) {
+		this.detalhe_dieta = detalhe_dieta;
+	}
+
+	public String getDetalhe_nutri() {
+		return detalhe_nutri;
+	}
+
+	public void setDetalhe_nutri(String detalhe_nutri) {
+		this.detalhe_nutri = detalhe_nutri;
+	}
+
+	public String getDetalhe_ergogenico() {
+		return detalhe_ergogenico;
+	}
+
+	public void setDetalhe_ergogenico(String detalhe_ergogenico) {
+		this.detalhe_ergogenico = detalhe_ergogenico;
 	}
 
 	public void setHoras_sono(Integer horas_sono) {
